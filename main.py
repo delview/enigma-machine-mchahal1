dde9905f8e20507d5e1e387224e9271e8955f29d
mchahal1 <manveerc077@deltalearns.ca>
2025-03-05 18:17:42 +0000

    started with basic psuedo code

---
+++ main.py
@@ -0,0 +1,4 @@
+# Ask user if they want to decrypt or encrypt a message
+# take string and call function to encrypt or decrypt
+# Create two functions that will accept parameters in the form of a list or dictionary that contains the str to be encrypted or decrypted
+# present new msg and allow user to repeat the program with that new msg
