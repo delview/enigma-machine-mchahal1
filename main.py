52ef8d354baaf1b896e543783c9ab24e324af9d6
mchahal1 <manveerc077@deltalearns.ca>
2025-03-06 21:04:41 +0000

    added bare bones for encryption function, working on decryption function

--- main.py
+++ main.py
@@ -4,23 +4,83 @@ def encrypter(msg):
         'b':12,
         'c':13,
         'd':14,
+        'e':15,
+        'f':21,
+        'g':22,
+        'h':23,
+        'i':24,
+        'j':24,
+        'k':25,
+        'l':31,
+        'm':32,
+        'n':33,
+        'o':34,
+        'p':35,
+        'q':41,
+        'r':42,
+        's':43,
+        't':44,
+        'u':45,
+        'v':51,
+        'w':52,
+        'x':53,
+        'y':54,
+        'z':55,
     }
     text = msg
     char_list = list(text)
-    for x in char_list():
+    for x in char_list:
         if x in enlist:
-            print('yes')
+            print(enlist[x])
         else:
             print("no")
-def decrypter(msg):
-    print("r")
+
+def decrypter(decryptlist):
+        delist = {
+        'a':11,
+        'b':12,
+        'c':13,
+        'd':14,
+        'e':15,
+        'f':21,
+        'g':22,
+        'h':23,
+        'i':24,
+        'j':24,
+        'k':25,
+        'l':31,
+        'm':32,
+        'n':33,
+        'o':34,
+        'p':35,
+        'q':41,
+        'r':42,
+        's':43,
+        't':44,
+        'u':45,
+        'v':51,
+        'w':52,
+        'x':53,
+        'y':54,
+        'z':55,
+    }
+        
+        for x in decryptlist:
+            if x in delist.values():
+                print(delist[x])
+            else:
+                print("no")
 # Ask user if they want to decrypt or encrypt a message
+decryptlist = []
 choice = input("Would you like to [e]ncrypt or [d]ecrypt a message? ")
-msg = input("Enter the message: ")
-# take string and call function to encrypt or decrypt
 if choice == 'e':
+    msg = input("Enter the message: ")
+# take string and call function to encrypt or decrypt
     encrypter(msg)
 if choice == 'd':
-    decrypter(msg)
+    numofgroups = int(input("How many groups of numbers are there?"))
+    for i in range(numofgroups):
+        decryptlist.append(input("Type a group: "))
+    decrypter(decryptlist)
 # Create two functions that will accept parameters in the form of a list or dictionary that contains the str to be encrypted or decrypted
 # present new msg and allow user to repeat the program with that new msg
